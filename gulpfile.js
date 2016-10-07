82ca2fae5ba3370454451fd0b8941230b2defaba
Ben Watson <ben@aceishigh.co.uk>
2016-10-07 08:39:59 +0100

    Added build task to Gulp and included icons

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var sass        = require('gulp-sass');
 var minifyCss   = require('gulp-minify-css');
 var include     = require('gulp-include');
 var watch       = require('gulp-watch');
+var del         = require('del');
+var gulpCopy    = require('gulp-copy');
+var merge       = require('merge-stream');
 
 
 gulp.task('sass', function () {
@@ -25,4 +28,29 @@ gulp.task('watch', function () {
     gulp.watch('./scripts/*.js', ['scripts'] );
 });
 
+//Build Dist
+gulp.task('clean', function () {
+    return del([
+        'WP-Places-I-Have-Been/**/*'
+    ]);
+});
+
+gulp.task('copy', function () {
+    var dist = gulp.src('./dist/*')
+        .pipe(gulpCopy('./WP-Places-I-Have-Been/'));
+    var flags = gulp.src('./flags/*')
+        .pipe(gulpCopy('./WP-Places-I-Have-Been/'));
+    var data = gulp.src('./data/*')
+        .pipe(gulpCopy('./WP-Places-I-Have-Been/'));
+    var assets = gulp.src('./assets/*')
+        .pipe(gulpCopy('./WP-Places-I-Have-Been/'));
+    var readme = gulp.src('./readme.txt')
+        .pipe(gulpCopy('./WP-Places-I-Have-Been/'));
+    var pihb = gulp.src('./WP-Places-I-Have-Been.php')
+        .pipe(gulpCopy('./WP-Places-I-Have-Been/'));
+    return merge(dist, flags, data, readme, pihb);
+});
+
 gulp.task('build', ['scripts', 'sass']);
+
+gulp.task('dist', ['clean', 'scripts', 'sass', 'copy']);
